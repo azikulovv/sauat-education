4ddc527720243e5644d205ea2c151c23e4214534
future <azikulov@proton.me>
2026-09-07 17:01:38 +0500

    feat(eslint): отключение правила для имен компонентов с несколькими словами

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -7,6 +7,7 @@ export default withNuxt({
     'vue/no-multiple-template-root': 'off',
     'vue/require-default-prop': 'off',
     'vue/html-self-closing': 'off',
+    'vue/multi-word-component-names': 'off',
 
     '@typescript-eslint/no-unused-vars': [
       'error',
