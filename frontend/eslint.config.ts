3653872a12c9639486f5f55577b7bdfdde37e952
future <azikulov@proton.me>
2026-09-07 14:02:49 +0500

    feat(eslint): отключение правил Vue для множественных корневых шаблонов и обязательных свойств

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -4,6 +4,9 @@ export default withNuxt({
   rules: {
     'no-console': 'warn',
     'no-debugger': 'error',
+    'vue/no-multiple-template-root': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/html-self-closing': 'off',
 
     '@typescript-eslint/no-unused-vars': [
       'error',
